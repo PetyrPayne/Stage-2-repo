a698c426a7c6e5dc7ea19073d5404c53c6334768
Payne <amugenp@gmail.com>
2018-04-09 00:58:16 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title> Rick's very own WEBSITE!</title>
+
+    <link rel="stylesheet" type="text/css" href="main.css">
+    
+</head>
+
+<body>
+
+    <p> Rick and Morty greets you at <span> 
+
+            <?php
+            echo date('Y-m-d H:i:s') . "<br>";
+            ?>
+            
+        
+          </span></p>
+
+    <div id="payne"></div>
+    <div id="rick"></div>
+    <div id="holder">
+            <div id="morty"></div>
+            <div id="jerry"></div>  
+    </div>
+
+
+    <p id="rick-text"> 
+
+            Rick: Listen, Jerry, I don't want to overstep my bounds or anything. It's your house. It's your world. You're a real Julius Caesar, but I'll tell you some, tell you how-how I feel about school, Jerry. It's a waste of time, a bunch of people running around, bumping into each other. G-guy up front says, "two plus two." The people in the back say, "four." Then the bell rings, and they give you a carton of milk and a piece of paper that says you can take a dump or something. I mean, it's—it's not a place for smart people, Jerry, and I know that's not a popular opinion, but it's my two cents on the issue.
+
+    </p>
+
+    <p id="copyright"> Copyright Rick and Morty </p>
+  
+
+</body>
+
+</html>
